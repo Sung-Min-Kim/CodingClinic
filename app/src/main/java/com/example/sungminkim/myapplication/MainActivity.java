a64042503b03c5f061398f35cb285cc76fdbe32c
Sung-Min-Kim <ksm3169@gmail.com>
2018-09-16 23:45:37 +0900

    First Homework

--- app/src/main/java/com/example/sungminkim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sungminkim/myapplication/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.sungminkim.myapplication;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+        final TextView txt = (TextView)findViewById(R.id.textview);
+        final ImageView img = (ImageView)findViewById(R.id.image);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("치킨도 먹고싶당");
+                img.setImageResource(R.drawable.chicken);
+            }
+        });
     }
 }
